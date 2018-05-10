6991ef2c0f22ab41d375192fe580e220e8325687
Gabor <gabor.benedek13@gmail.com>
2018-05-10 09:44:44 +0300

    Sa se calculeze media aritmetica a doua numere.

---
+++ P2.java
@@ -0,0 +1,11 @@
+public class P2 {
+    public static void main(String[] args) {
+
+        int a =31;
+        int b=28;
+        int sum=a+b;
+        double MA = sum/2d;
+        System.out.println(MA);
+
+    }
+}
